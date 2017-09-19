922d71eb23f823d4e1cdd9bd4a178e48e7d7115e
zpottie <zander.potgieter@learningworks.co.nz>
2017-09-19 13:02:19 +1200

    Class cleanup - reduced duplicated code

--- classes/restore_table.php
+++ classes/restore_table.php
@@ -29,8 +29,8 @@ global $CFG;
 
 // Load Moodle config.
 require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
-// Load Tablelib lib.
-require_once($CFG->dirroot .'/lib/tablelib.php');
+// Load parent (& tablelib lib).
+require_once(dirname(__FILE__) . '/base_table.php');
 
 require_login();
 
@@ -40,174 +40,7 @@ require_login();
  * @copyright  2016 onwards LearningWorks Ltd {@link https://learningworks.co.nz/}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class advnotifications_restore_table extends table_sql {
-
-    // Lang strings that get re-used below is stored in variables to improve efficiency (Don't have to get strings many times).
-    /**
-     * @var null|string
-     */
-    private $yes = null;
-    /**
-     * @var null|string
-     */
-    private $no = null;
-
-    /**
-     * Constructor
-     * @param int $uniqueid all tables have to have a unique id, this is used
-     *      as a key when storing table properties like sort order in the session.
-     */
-    public function __construct($uniqueid) {
-        parent::__construct($uniqueid);
-        // Define the list of columns to show.
-        $columns = array(
-            'id',
-            'title',
-            'type',
-            'enabled',
-            'global',
-            'aicon',
-            'dismissible',
-            'times',
-            'date_from',
-            'date_to',
-            'actions'
-        );
-        $this->define_columns($columns);
-
-        // Define the titles of columns to show in header from lang file.                           // Examples.
-        $headers = array(
-            get_string('advnotifications_field_id', 'block_advnotifications'),          // Id: 1.
-            get_string('advnotifications_field_title', 'block_advnotifications'),       // Title: Site Maintenance.
-            get_string('advnotifications_field_type', 'block_advnotifications'),        // Type: info.
-            get_string('advnotifications_field_enabled', 'block_advnotifications'),     // Enabled: Yes.
-            get_string('advnotifications_field_global', 'block_advnotifications'),      // Global: Yes.
-            get_string('advnotifications_field_aicon', 'block_advnotifications'),        // AIcon: Yes.
-            get_string('advnotifications_field_dismissible', 'block_advnotifications'), // Dismissible: Yes.
-            get_string('advnotifications_field_times', 'block_advnotifications'),       // Times: 10.
-            get_string('advnotifications_field_date_from', 'block_advnotifications'),   // Date From: dd/mm/yyyy.
-            get_string('advnotifications_field_date_to', 'block_advnotifications'),     // Date To: dd/mm/yyyy.
-            get_string('advnotifications_field_actions', 'block_advnotifications'),     // Actions: Edit | Delete.
-        );
-        $this->define_headers($headers);
-
-        $this->sortable(true, 'id', SORT_DESC);
-        $this->no_sorting('actions');
-
-        // Lang string initialisation.
-        $this->yes = get_string('advnotifications_cell_yes', 'block_advnotifications'); // Yes.
-        $this->no = get_string('advnotifications_cell_no', 'block_advnotifications');   // No.
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * id value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return integer Returns notification ids - easier sorting
-     */
-    public function col_id($values) {
-        return $values->id;
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * title value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Returns notification's title - easier sorting
-     */
-    public function col_title($values) {
-        return $values->title;
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * type value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return notification type (for styling purposes)
-     */
-    public function col_type($values) {
-        return $values->type;
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * enabled value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return whether notification is enabled or not
-     */
-    public function col_enabled($values) {
-        return ($values->enabled == 1 ? $this->yes : $this->no);
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * global value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return whether notification will be propogated globally/site-wide or not
-     */
-    public function col_global($values) {
-        return ($values->global == 1 ? $this->yes : $this->no);
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * aicon value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return whether notification is to display an icon or not
-     */
-    public function col_aicon($values) {
-        return ($values->aicon == 1 ? $this->yes : $this->no);
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * dismissible value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return whether notification is dismissible or not
-     */
-    public function col_dismissible($values) {
-        return ($values->dismissible == 1 ? $this->yes : $this->no);
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * times value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return $string Return number of times the user should view the notification
-     */
-    public function col_times($values) {
-        return $values->times;
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * date_from value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return integer Return value from when the notification should be displayed
-     */
-    public function col_date_from($values) {
-        return date('d/m/Y', $values->date_from);
-    }
-
-    /**
-     * This function is called for each data row to allow processing of the
-     * date_to value.
-     *
-     * @param object $values Contains object with all the values of record.
-     * @return integer Return value until when the notification should be displayed
-     */
-    public function col_date_to($values) {
-        return date('d/m/Y', $values->date_to);
-    }
+class advnotifications_restore_table extends advnotifications_base_table {
 
     /**
      * This function is called for each data row to allow processing of the
@@ -239,14 +72,5 @@ class advnotifications_restore_table extends table_sql {
                 get_string('advnotifications_delete_label', 'block_advnotifications') . '">
                 </form>';
         }
-    }
-
-    /**
-     * This function is not part of the public api.
-     */
-    public function print_nothing_to_display() {
-        $this->print_initials_bar();
-
-        echo '<p class="notifications--empty">' . get_string('advnotifications_table_empty', 'block_advnotifications') . '</p>';
     }
 }
