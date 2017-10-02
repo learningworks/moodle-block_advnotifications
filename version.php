6fa669917d1be4522e9dee190b1eb13ccb8ef1bf
zpottie <zander.potgieter@learningworks.co.nz>
2017-10-02 17:16:05 +1300

    Fixes version file reqs

--- version.php
+++ version.php
@@ -26,6 +26,8 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_advnotifications';          // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
-$plugin->version = 201607071321;                        // YYYYMMDDHH (year, month, day, 24-hr format hour).
+$plugin->version = 2017100217;                        // YYYYMMDDHH (year, month, day, 24-hr format hour).
 $plugin->requires = 2015051104;                         // YYYYMMDDHH (This is the stable version for Moodle 2.9 as at 04/07/2016).
 $plugin->cron = 24 * 3600;                              // Cron interval 1 day.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1.0.1';
