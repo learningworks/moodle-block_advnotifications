6dc7f59a070866b73da2748a6b6bd9557847bc40
Daniel Neis Araujo <danielneis@gmail.com>
2021-09-20 16:01:44 -0300

    add option to send notification (but not send them yet)

--- db/upgrade.php
+++ db/upgrade.php
@@ -159,6 +159,15 @@ function xmldb_block_advnotifications_upgrade($oldversion) {
             $dbman->create_table($table);
         }
 
+        // Define field id to be added to block_advnotifications.
+        $table = new xmldb_table('block_advnotifications');
+        $field = new xmldb_field('sendnotifications', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'aicon');
+
+        // Conditionally launch add field id.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
         // Advnotifications savepoint reached.
         upgrade_block_savepoint(true, 2021101400, 'advnotifications');
     }
